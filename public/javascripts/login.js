f997ad1a473928108176273333e78e3f914d7189
dingxing <dingxing@skyworth.com>
2017-01-03 16:34:09 +0800

    dingxing

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -9,15 +9,7 @@ function loginfun() {
     var node = '{"data":{"username":"' + username + '","password":"' + pwd + '"}}';
     console.log(node);
     if (username != "" && pwd != "") {
-<<<<<<< HEAD
         sendHTTPRequest("/users", node, loginresult);
-        // sendHTTPRequest("http://127.0.0.1:3000/api/login", node, loginresult);
-    }
-    else {
-        console.log("请填写完整")
-    }
-=======
-        sendHTTPRequest("http://172.20.132.225:3000/api/login", node, loginresult);
     }
     else if(username == ""){
         var usermessage = document.getElementById('usermessage');
@@ -30,7 +22,6 @@ function loginfun() {
         setTimeout("pwdmessage.innerHTML=''",2000);
     }
 
->>>>>>> b183aed8703acf1f402e4d4f5978533ea2ff3d3b
 }
 
 function loginresult() {
@@ -43,12 +34,7 @@ function loginresult() {
             var data = JSON.parse(this.responseText);
             if (data.msg == "success") {
                 level = 1;
-<<<<<<< HEAD
-                document.location.href = "index.html";
 
-            }
-            ;
-=======
                 document.location.href="index.html" ;
                 
             }
@@ -57,7 +43,7 @@ function loginresult() {
             loginmsg.innerHTML = loginmsg.innerHTML+"!请输入正确账号或密码";
             setTimeout("document.getElementById('logintxt').innerHTML='　'",2000);
 	    };
->>>>>>> b183aed8703acf1f402e4d4f5978533ea2ff3d3b
+
             // loginId = data.data;
             // printlog(loginId);
 
