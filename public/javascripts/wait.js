95442b1f902c9f0a1a8d92fb8274fa70e579eaa8
xavier0509 <xiejr_work@163.com>
2017-01-05 17:29:17 +0800

    modify: show tables for start info

--- public/javascripts/wait.js
+++ public/javascripts/wait.js
@@ -31,30 +31,36 @@ function searchResource() {
 				title.removeChild(title.childNodes[i]); //删除掉每个子节点的内容
 			};			
 			var data = JSON.parse(this.responseText);
-			var datalength = data.data;
-			console.log(datalength);
-			for (var i = 0; i < datalength.length; i++) {
-				var objData = datalength[i].DevInfo;
-				console.log(objData);
-				for(var j = 0; j < objData.length; j++) {
-					_row = document.getElementById("wait-tablebody").insertRow(0);
-					var _cell0 = _row.insertCell(0);
-					_cell0.innerHTML = "<input type='checkbox' class='checkboxstatus' value=''>";
-					var _cell1 = _row.insertCell(1);
-					_cell1.innerHTML = objData[j].platformModel;
-					var _cell2 = _row.insertCell(2);
-					_cell2.innerHTML = objData[j].productModel;
-					var _cell3 = _row.insertCell(3);
-					_cell3.innerHTML = objData[j].androidVersion;
-					var _cell3 = _row.insertCell(4);
-					_cell3.innerHTML = objData[j].chipModel;
-					var _cell3 = _row.insertCell(5);
-					_cell3.innerHTML = objData[j].memorySize;
-					var _cell4 = _row.insertCell(6);
-					_cell4.innerHTML = "<div class='btn-group'><button type='button' class='btn btn-default eachedit'>编辑</button><button type='button' class='btn btn-default eachdelete'>删除</button><button type='button' class='btn btn-default eachcopy'>复制</button></div>";
+			var msg = data.msg;
+			if (msg == "success") {
+				var datalength = data.data;
+				console.log(datalength);
+				for (var i = 0; i < datalength.length; i++) {
+					var objData = datalength[i].DevInfo;
+					console.log(objData);
+					for(var j = 0; j < objData.length; j++) {
+						_row = document.getElementById("wait-tablebody").insertRow(0);
+						var _cell0 = _row.insertCell(0);
+						_cell0.innerHTML = "<input type='checkbox' class='checkboxstatus' value=''>";
+						var _cell1 = _row.insertCell(1);
+						_cell1.innerHTML = objData[j].platformModel;
+						var _cell2 = _row.insertCell(2);
+						_cell2.innerHTML = objData[j].productModel;
+						var _cell3 = _row.insertCell(3);
+						_cell3.innerHTML = objData[j].androidVersion;
+						var _cell3 = _row.insertCell(4);
+						_cell3.innerHTML = objData[j].chipModel;
+						var _cell3 = _row.insertCell(5);
+						_cell3.innerHTML = objData[j].memorySize;
+						var _cell4 = _row.insertCell(6);
+						_cell4.innerHTML = "<div class='btn-group'><button type='button' class='btn btn-default eachedit'>编辑</button><button type='button' class='btn btn-default eachdelete'>删除</button><button type='button' class='btn btn-default eachcopy'>复制</button></div>";
+					};
 				};
-			};
+			}
+			else{
+				//查询为空
 
+			}
 		}
 	}
 }
