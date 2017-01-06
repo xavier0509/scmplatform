063a362f8b088334452700f6868e305db6ba76ae
dingxing <dingxing@skyworth.com>
2017-01-06 18:13:57 +0800

    add interface /api/session

--- app.js
+++ app.js
@@ -3,29 +3,35 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+
+// 将 cookie 信息保存到 mongodb 中
 var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+var MongoStore = require('connect-mongo')(session);
+var setting = require('./setting');
+// var FileStore = require('session-file-store')(session);
+
 
 var bodyParser = require('body-parser');
 var consolidate = require('consolidate');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
 
+
 var app = express();
 
 
- // 允许跨越请求
- // app.all('*', function(req, res, next) {
- // res.header("Access-Control-Allow-Origin", "*");
- // res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
- // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
- // res.header("X-Powered-By",' 3.2.1');
- // if(req.method=="OPTIONS") res.sendStatus(200);
- // else  next();
- // });
-
+// 允许跨越请求
+// app.all('*', function(req, res, next) {
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+// res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+// res.header("X-Powered-By",' 3.2.1');
+// if(req.method=="OPTIONS") res.sendStatus(200);
+// else  next();
+// });
 
 
 // view engine setup
@@ -46,19 +52,60 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// session store
 app.use(session({
-    store: new FileStore(),  // 将session存储到一个文件中，比如浏览器关闭后session无效的问题
-    cookie: {maxAge: 2000 * 1000},  // 过期时间20秒
-    secret: "dingxing"
+    secret: "123",
+    key: 'sid',
+    store: new MongoStore({
+        url: 'mongodb://localhost/session',
+    }),
+    resave: true,
+    saveUninitialized: true,
 }));
 
 
+/*app.use(session({
+ // store: new FileStore(),  // 将session存储到一个文件中，比如浏览器关闭后session无效的问题
+ // cookie: {maxAge: 2000 * 1000},  // 过期时间20秒
+ // secret: "dingxing"
 
+
+ }));*/
+
+
+/*app.use(session({
+ secret: 'myblog',
+ key: "cookie11111",//cookie name
+ cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+ resave: false,
+ saveUninitialized: true,
+ store: new MongoStore({
+ // url: db_config.module.dbUrl //这里就是coding连接信息的uri
+ url: 'mongodb://localhost/session'
+ })
+
+ }));*/
+
+
+/*var sessionStore = new MongoStore({
+ host: '127.0.0.1',
+ port: '27017',
+ db: 'session',
+ url: 'mongodb://localhost:27017/demo'
+ });
+
+ //session store:
+ app.use(express.session({
+ secret: 'my secret sign key',
+ store: sessionStore
+ }));*/
 
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/api', api);
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
