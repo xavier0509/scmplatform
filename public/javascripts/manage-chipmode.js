0226f940fc45b4cc94504df23129237894caea8e
linxinwang <linxinwang@source.skyworth.com:29419>
2017-01-06 15:29:06 +0800

    lxw-010602

--- public/javascripts/manage-chipmode.js
+++ public/javascripts/manage-chipmode.js
@@ -21,17 +21,17 @@ function AfterChipModeHtmlInfo() {
 			$('#myModeChipAddModal').modal();
 			$(".modal-backdrop").addClass("new-backdrop");
 		}
-	/*机芯机型板块-机芯-修改*/
+		/*机芯机型板块-机芯-修改*/
 	var oTableA = $("#chip-mkTable").find("a");
 	console.log(oTableA.length);
 	for(var i = 0; i < oTableA.length; i++) {
 		oTableA[i].index = i;
 		oTableA[i].onclick = function() {
-			console.log("ok" + this.index); //点击的是第几个
-			$('#myModeChipAddModal').modal(); //显示新建与编辑机芯机型时的弹框
-			$(".modal-backdrop").addClass("new-backdrop");
-		}
-		//给保存按钮传参
+				console.log("ok" + this.index); //点击的是第几个
+				$('#myModeChipAddModal').modal(); //显示新建与编辑机芯机型时的弹框
+				$(".modal-backdrop").addClass("new-backdrop");
+			}
+			//给保存按钮传参
 		toSaveButton(this.index);
 	}
 	/*机芯机型板块-机型-修改*/
@@ -49,15 +49,14 @@ function AfterChipModeHtmlInfo() {
 	var hipOrModeClose = document.getElementById("inputChipOrModeClose");
 	hipOrModeClose.onclick = function() {
 			console.log("lxw " + "in inputChipOrModeClose");
-	}
-	/*机芯机型板块-机芯-增加-保存*/
-	function toSaveButton(index){
+		}
+		/*机芯机型板块-机芯-增加-保存*/
+	function toSaveButton(index) {
 		var ChipOrModeSubmit = document.getElementById("inputChipOrModeSubmit");
 		ChipOrModeSubmit.onclick = function() {
 			console.log("lxw " + "in inputChipOrModeSubmit");
 		}
 	}
-	
 
 }
 
@@ -65,47 +64,69 @@ function AfterChipModeHtmlInfo() {
 function ChipModeHtmlInfo() {
 	console.log("lxw " + "ChipModeHtmlInfo");
 	console.log("this.readyState = " + this.readyState);
-    if (this.readyState == 4) {
-        console.log("this.status = " + this.status);
-        console.log("this.responseText = " + this.responseText);
-        if (this.status == 200) //TODO
-        {
-        	var data = JSON.parse(this.responseText);
-            console.log("lxw "+data);
-        };
-        // loginId = data.data;
-        // printlog(loginId);
-        }
-    }
-	
-	var currentData = {
-		"chip":[{"chipName":"8S61"}
-			,{"chipName":"8S62"}
-			,{"chipName":"8S63"}
-			,{"chipName":"8S64"}
-			,{"chipName":"8S65"}
-			,{"chipName":"8S66"}
-			,{"chipName":"8S67"}
-			,{"chipName":"8S68"}
-			,{"chipName":"8S69"}
-			,{"chipName":"8S60"}]
-		,"model":[{"modelName":"A01"}
-			,{"modelName":"A02"}
-			,{"modelName":"A03"}
-			,{"modelName":"A04"}
-			,{"modelName":"A05"}
-			,{"modelName":"A06"}
-			,{"modelName":"A07"}
-			,{"modelName":"A08"}
-			,{"modelName":"A09"}
-			,{"modelName":"A10"}]};
-	console.log("lxw " + currentData.chip.length);
-	var _rowChip = document.getElementById("chipManageAdd-td");
-	for(var i = 0; i < currentData.chip.length; i++) {
-		_rowChip.innerHTML += "<div class='col-xs-4'><a>"+currentData.chip[i].chipName+"</a></div>";
+	if(this.readyState == 4) {
+		console.log("this.status = " + this.status);
+		console.log("this.responseText = " + this.responseText);
+		if(this.status == 200) //TODO
+		{
+			var data = JSON.parse(this.responseText);
+			console.log("lxw " + data);
+		};
+		// loginId = data.data;
+		// printlog(loginId);
 	}
-	var _rowModal = document.getElementById("modalManageAdd-td");
-	for(var i = 0; i < currentData.model.length; i++) {
-		_rowModal.innerHTML += "<div class='col-xs-4'><a>"+currentData.model[i].modelName+"</a></div>";
-	}
+}
+
+var currentData = {
+	"chip": [{
+		"chipName": "8S61"
+	}, {
+		"chipName": "8S62"
+	}, {
+		"chipName": "8S63"
+	}, {
+		"chipName": "8S64"
+	}, {
+		"chipName": "8S65"
+	}, {
+		"chipName": "8S66"
+	}, {
+		"chipName": "8S67"
+	}, {
+		"chipName": "8S68"
+	}, {
+		"chipName": "8S69"
+	}, {
+		"chipName": "8S60"
+	}],
+	"model": [{
+		"modelName": "A01"
+	}, {
+		"modelName": "A02"
+	}, {
+		"modelName": "A03"
+	}, {
+		"modelName": "A04"
+	}, {
+		"modelName": "A05"
+	}, {
+		"modelName": "A06"
+	}, {
+		"modelName": "A07"
+	}, {
+		"modelName": "A08"
+	}, {
+		"modelName": "A09"
+	}, {
+		"modelName": "A10"
+	}]
+};
+console.log("lxw " + currentData.chip.length);
+var _rowChip = document.getElementById("chipManageAdd-td");
+for(var i = 0; i < currentData.chip.length; i++) {
+	_rowChip.innerHTML += "<div class='col-xs-4'><a>" + currentData.chip[i].chipName + "</a></div>";
+}
+var _rowModal = document.getElementById("modalManageAdd-td");
+for(var i = 0; i < currentData.model.length; i++) {
+	_rowModal.innerHTML += "<div class='col-xs-4'><a>" + currentData.model[i].modelName + "</a></div>";
 }
