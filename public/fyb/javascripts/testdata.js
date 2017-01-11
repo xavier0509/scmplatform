c20793258278239f0996f71139e45dbea52cf4c5
linxinwang <linxinwang@source.skyworth.com:29419>
2017-01-11 17:10:58 +0800

    lxw-011103

---
+++ public/fyb/javascripts/testdata.js
@@ -0,0 +1,26 @@
+document.write("<script language=javascript src='../javascripts/sentHTTP.js' charset=\"utf-8\"></script>");
+
+var myButton = document.getElementById("ensure");
+
+myButton.onclick = function(){
+	var mysrc = document.getElementById("interface").value;
+	var mypara = document.getElementById("parameter").value;
+	var myreturn = document.getElementById("return").value;
+	
+	sendHTTPRequest(mysrc, '{"data":""}', returnInfo);
+}
+
+function returnInfo(){
+	console.log("this.readyState = " + this.readyState);
+    if (this.readyState == 4) {
+        console.log("this.status = " + this.status);
+        console.log("this.responseText = " + this.responseText);
+        if (this.status == 200) //TODO
+        {
+            var mydata = JSON.parse(this.responseText);
+            console.log("lxw "+ mydata);
+        }
+    }
+}
+
+
