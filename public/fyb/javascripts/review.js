72123cbb30482b2ef1777b60554857f13a90296d
xavier0509 <xiejr_work@163.com>
2017-01-18 10:01:36 +0800

    modify:待审核页面的编辑提交

--- public/fyb/javascripts/review.js
+++ public/fyb/javascripts/review.js
@@ -1,6 +1,6 @@
 document.write("<script language=javascript src='../javascripts/sentHTTP.js' charset=\"utf-8\"></script>");
 document.write("<script language=javascript src='../javascripts/login.js' charset=\"utf-8\"></script>");
-
+//加载自执行，传递参数请求列表
 $(function () {
     var level = parent.adminFlag;
     var loginusername = parent.loginusername;
@@ -16,6 +16,7 @@ $(function () {
 var chip = null;
 var model = null;
 
+//在待审核页面出现列表
 function reviewlist(){
     console.log("this.readyState = " + this.readyState);
     if (this.readyState == 4) {
@@ -61,7 +62,7 @@ function reviewlist(){
 
                         }
                         else{
-                            _cell6.innerHTML = "<div class='btn-group'><button type='button' class='btn btn-default' onclick='edit(this)'>编辑</button></div>";
+                            _cell6.innerHTML = "<div class='btn-group'><button type='button' class='btn btn-default' onclick='review(this)'>编辑</button></div>";
 
                         }
                     }
@@ -84,17 +85,21 @@ function recover(obj){
 
 }
 
+//恢复的回调函数
 function recoverResult(){
 
 }
 
+//点击编辑、审核出现页面的执行函数
 function review(obj){
     chip = obj.parentNode.parentNode.parentNode.children[0].innerHTML;
     model = obj.parentNode.parentNode.parentNode.children[1].innerHTML;
-    sendHTTPRequest("/fyb_api/moduleQuery", '{"data":{}}', moduleResult);    //查询模块信息接口
-    // sendHTTPRequest("/reviewcontent", '{"data":{"platformModel":"'+chip+'","productModel":"'+model+'"}}', reviewresult);
+    //查询模块信息接口
+    sendHTTPRequest("/fyb_api/moduleQuery", '{"data":{}}', moduleResult);    
+    
 }
 
+//罗列出所有的mk信息
 function moduleResult(){
     if (this.readyState == 4) {
         // console.log("this.status = " + this.status);
@@ -145,6 +150,9 @@ function moduleResult(){
                     var input = document.createElement("input");
                     input.setAttribute('value',name[i].engName);
                     input.setAttribute('id',name[i].engName);
+                    input.setAttribute('category',name[i].category);
+                    input.setAttribute('gitPath',name[i].gitPath);
+                    input.setAttribute('desc',name[i].desc);
                     input.setAttribute('type','checkbox');
                     // if (name[i].state == 1) {input.setAttribute('checked','');};//勾选状态
                     child.appendChild(input);
@@ -157,65 +165,6 @@ function moduleResult(){
     sendHTTPRequest("/fyb_api/productQuery", '{"data":{"condition":{"chip":"'+chip+'","model":"'+model+'"},"option":{}}}', reviewresult);   
     }
 }
-
-
-// function configResult(){
-//     if (this.readyState == 4) {
-//         // console.log("this.status = " + this.status);
-//         // console.log("this.responseText = " + this.responseText);
-//         if (this.status == 200) //TODO
-//         {
-//             var main = [];
-//             var other = [];
-
-//             var data = JSON.parse(this.responseText);
-//             for (var i = 0; i < data.data.length; i++) {
-//                 if(data.data[i].category == "main"){main.push(data.data[i]);}
-//                 else if (data.data[i].category == "other") {other.push(data.data[i]);}
-//             };
-
-//             document.getElementById("maincont").innerHTML="";
-//             document.getElementById("othercont").innerHTML="";
-
-//             creatConfig(main,"maincont");
-//             creatConfig(other,"othercont");
-
-//             function creatConfig(name,divname){
-//                 for (var i = 0; i < name.length; i++) {
-//                     var cont = document.getElementById(divname);
-//                     var child = document.createElement("div");
-//                     child.setAttribute('class','col-sm-5 form-group text-right');
-//                     var text = document.createTextNode(name[i].cnName+"("+name[i].engName+")　");
-//                     if (name[i].type == "string") {
-//                         var input = document.createElement("input");
-//                         input.setAttribute("title",name[i].engName);
-//                         input.value = name[i].value;
-//                     }
-//                     else if (name[i].type == "enum"){
-//                         var input = document.createElement("select");
-//                         input.setAttribute("title",name[i].engName);
-//                         input.setAttribute("class","form-group");
-//                         for (var j = 0; j< name[i].options.length; j++) {
-//                             var txt = name[i].options[j];
-//                             var option =document.createElement("option");
-//                             option.setAttribute("value",txt);
-//                             if (txt == name[i].value) {
-//                                 option.setAttribute("selected","")
-//                             };
-//                             var txtvalue = document.createTextNode(txt);
-//                             option.appendChild(txtvalue);
-//                             input.appendChild(option);
-//                         };
-//                     }
-//                     child.appendChild(text);
-//                     child.appendChild(input);
-//                     cont.appendChild(child);
-//                 }
-//             }
-//         }
-//     sendHTTPRequest("/fyb_api/productQuery", '{"data":{"condition":{"chip":"'+chip+'","model":"'+model+'"},"option":{}}}', reviewresult);   
-//     }    
-// }
 
 
 function reviewresult(){
@@ -241,7 +190,7 @@ function reviewresult(){
             document.getElementById("memory").value=data.data[0].memorySize;
 
 
-            //更新mk文件信息
+            //更新mk文件信息，匹配后勾选
             var mkfile = data.data[0].mkFile;
             for (var i = 0; i < mkfile.length; i++) {
                 document.getElementById(mkfile[i].engName).setAttribute('checked','');
@@ -310,11 +259,94 @@ function reviewresult(){
                 for (var i = 0; i < selectcounts.length; i++) {
                     selectcounts[i].setAttribute('disabled','')
                 }
+                document.getElementById("reviewSubmit").innerHTML = "审核通过";
+                document.getElementById("btn_submit").onclick = function(){
+                    sendHTTPRequest("/fyb_api/productUpdate",'{"data":{"condition":{"chip":"'+chip+'","model":"'+model+'"},"action":"set","update":{"operateType":"0","gerritState":"0"}}}',passResult);
+                }
+                
+            }
+            else{
+                document.getElementById("reviewSubmit").innerHTML = "提交";
+                document.getElementById("btn_submit").onclick = reviewEdit;
             }
             
         }
     }
 }
+
+//点击审核通过的回调
+function passResult(){
+
+}
+
+//点击编辑提交的函数
+function reviewEdit(){
+    console.log("开始提交数据");
+
+    var dataarry = [];//用于存放全部数据（包括mk和config）
+    getmkdata("#appcont");
+    getmkdata("#appstorecont");
+    getmkdata("#homecont");
+    getmkdata("#imecont");
+    getmkdata("#servicecont");
+    getmkdata("#syscont");
+    getconfigdata("#maincont");
+    getconfigdata("#othercont");
+
+    //获取mk文件数据
+    function getmkdata(name){
+        $size = $(name).find("div");
+        // if ($size.length!=0) {console.log($size);}else{console.log("ddd")};        
+        var array1 = [];//用于存放当前name分类下的数据
+        for (var i = 0; i < $size.length; i++) {
+            if($size[i].childNodes[0].checked){
+                var cnName = $size[i].innerText;
+                var engName = $size[i].childNodes[0].value;
+                var desc = $size[i].childNodes[0].desc;
+                var gitPath = $size[i].childNodes[0].gitPath;
+                var category = $size[i].childNodes[0].category;
+                var data = '{"cnName":"'+cnName+'","desc":"'+desc+'","engName":"'+engNname+'","gitPath":"'+gitPath+'","category":"'+category+'"}';
+                console.log(data);
+                array1.push(JSON.parse(data));//将当前name分类下的数据存到数组中
+            }
+        };
+        console.log(JSON.stringify(array));
+        dataarry.push(array1);//将分类之后的数组存到一个数组中
+        console.log(dataarry[0]);
+        console.log(JSON.stringify(dataarry));
+    }
+
+    //获取config文件数据
+    // function getconfigdata(name){
+    //     $size = $(name).find("div");
+    //     var array2 = [];
+    //     for (var i = 0; i < $size.length; i++) {
+    //         var pkgname = $size[i].childNodes[1].title;
+    //         var value = $size[i].childNodes[1].value;
+    //         var data = '{"pkgname":"'+pkgname+'","value":"'+value+'"}';
+    //         console.log(data);
+    //         array2.push(JSON.parse(data));
+    //     };
+    //     dataarry.push(array2);
+    //     console.log(dataarry);
+    // }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 $('#configbutton').click(function(){
