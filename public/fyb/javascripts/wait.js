205e84a055759097932af75cc6f02c304c575977
linxinwang <linxinwang@source.skyworth.com:29419>
2017-01-18 21:32:56 +0800

    lxw-0118106

--- public/fyb/javascripts/wait.js
+++ public/fyb/javascripts/wait.js
@@ -1059,6 +1059,7 @@ function getCopyInforesult() {
 					console.log("lxw " + data.data[0].mkFile[i].engName);
 					//document.getElementById(data.data[0].mkFile[i].engName).checked = true;
 					document.getElementById(data.data[0].mkFile[i].engName).setAttribute('checked', '');
+					console.log("lxw "+document.getElementById(data.data[0].mkFile[i].engName).checked);
 				}
 				console.log("lxw " + data.data[0].configFile.length); //config
 				for(var i = 0; i < data.data[0].configFile.length; i++) {
