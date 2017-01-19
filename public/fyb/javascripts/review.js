9ab5e56f27de261b4feec045efcbe58cbeeac0c9
xavier0509 <xiejr_work@163.com>
2017-01-19 18:05:51 +0800

    modify: 重新获取config，不能直接通过机芯机型查询

--- public/fyb/javascripts/review.js
+++ public/fyb/javascripts/review.js
@@ -154,8 +154,8 @@ function moduleResult(){
             var sysapp = [];
             var tv = [];
             var mkother = [];
-            var main = [];
-            var other = [];
+            // var main = [];
+            // var other = [];
 
             var data = JSON.parse(this.responseText);
             for (var i = 0; i < data.data.length; i++) {
@@ -208,7 +208,112 @@ function moduleResult(){
             }
         }
     // 查询对应机芯机型的配置信息
-    sendHTTPRequest("/fyb_api/productQuery", '{"data":{"condition":{"chip":"'+chip+'","model":"'+model+'"},"option":{}}}', reviewresult);   
+    // sendHTTPRequest("/fyb_api/productQuery", '{"data":{"condition":{"chip":"'+chip+'","model":"'+model+'"},"option":{}}}', reviewresult);   
+    //查询config信息接口
+    sendHTTPRequest("/fyb_api/configQuery", '{"data":{}}', configResult); 
+    }
+}
+
+function configResult(){
+    if (this.readyState == 4) {
+        // console.log("this.status = " + this.status);
+        console.log("this.responseText = " + this.responseText);
+        if (this.status == 200) //TODO
+        {
+            var data = JSON.parse(this.responseText);
+            var configfile = data.data;
+            var main = [];
+            var other = [];
+            var hardware = [];
+            var ad = [];
+            var channel = [];
+            var localmedia = [];
+            var browser = [];
+            var serverip = [];
+
+            for (var i = 0; i < configfile.length; i++) {
+                if(configfile[i].category == "main"){main.push(configfile[i]);}
+                else if (configfile[i].category == "other") {other.push(configfile[i]);}
+                else if (configfile[i].category == "hardware") {hardware.push(configfile[i]);}
+                else if (configfile[i].category == "serverip") {serverip.push(configfile[i]);}
+                else if (configfile[i].category == "ad") {ad.push(configfile[i]);}
+                else if (configfile[i].category == "channel") {channel.push(configfile[i]);}
+                else if (configfile[i].category == "localmedia") {localmedia.push(configfile[i]);}
+                else if (configfile[i].category == "browser") {browser.push(configfile[i]);}
+            };
+
+            document.getElementById("maincont").innerHTML="";
+            document.getElementById("hardwarecont").innerHTML="";
+            document.getElementById("serveripcont").innerHTML="";
+            document.getElementById("adcont").innerHTML="";
+            document.getElementById("channelcont").innerHTML="";
+            document.getElementById("localmediacont").innerHTML="";
+            document.getElementById("browsercont").innerHTML="";
+            document.getElementById("othercont").innerHTML="";
+
+            creatConfig(main,"maincont");
+            creatConfig(other,"othercont");
+            creatConfig(hardware,"hardwarecont");
+            creatConfig(serverip,"serveripcont");
+            creatConfig(ad,"adcont");
+            creatConfig(channel,"channelcont");
+            creatConfig(localmedia,"localmediacont");
+            creatConfig(browser,"browsercont");
+
+            function creatConfig(name,divname){
+                for (var i = 0; i < name.length; i++) {
+                    var cont = document.getElementById(divname);
+                    var child = document.createElement("div");
+                    child.setAttribute('class','col-sm-5 form-group text-right');
+                    var text = document.createTextNode(name[i].cnName);
+                    if (name[i].type == "string") {
+                        var input = document.createElement("input");
+                        input.setAttribute("engName",name[i].engName);
+                        input.setAttribute("configKey",name[i].configKey);
+                        input.setAttribute("type",name[i].type);
+                        input.setAttribute("desc",name[i].desc);
+                        input.setAttribute("category",name[i].category);
+                        input.setAttribute("options",name[i].options);
+                        input.setAttribute("cnName",name[i].cnName);
+                        input.value = name[i].value;
+                    }
+                    else if (name[i].type == "enum"){
+                        var opt = [];
+                        for (var j = 0; j < name[i].options.length; j++) {
+                            opt.push(name[i].options[j]);
+                            console.log(opt);
+                        };
+                        var input = document.createElement("select");
+                        input.setAttribute("engName",name[i].engName);
+                        input.setAttribute("configKey",name[i].configKey);
+                        input.setAttribute("type",name[i].type);
+                        input.setAttribute("desc",name[i].desc);
+                        input.setAttribute("category",name[i].category);
+                        input.setAttribute("options",opt);
+                        input.setAttribute("cnName",name[i].cnName);
+                        input.setAttribute("value",name[i].value);
+                        input.setAttribute("class","form-group");
+                        for (var j = 0; j< name[i].options.length; j++) {
+                            var txt = name[i].options[j];
+                            var option =document.createElement("option");
+                            option.setAttribute("value",txt);
+                            if (txt == name[i].value) {
+                                option.setAttribute("selected","")
+                            };
+                            var txtvalue = document.createTextNode(txt);
+                            option.appendChild(txtvalue);
+                            input.appendChild(option);
+                        };
+                    }
+                    child.appendChild(text);
+                    child.appendChild(input);
+                    cont.appendChild(child);
+                }
+            }
+
+            
+
+        }
     }
 }
 
