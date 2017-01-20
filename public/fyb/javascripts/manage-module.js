981f1cac2492b356e17cb9fd50824f0f18d9c11a
xavier0509 <xiejr_work@163.com>
2017-01-20 18:14:34 +0800

    modify: 模块管理隐藏所有数据

--- public/fyb/javascripts/manage-module.js
+++ public/fyb/javascripts/manage-module.js
@@ -18,13 +18,14 @@ function AfterModuleHtmlInfo() {
 	/*机芯机型板块-机型-修改------------这里需要分级------ table-tr-a   */
 	var oTableA = $("#module-mkTable").find("a");
 	var oTableInput = $("#module-mkTable").find("input");
+	console.log("xjr"+oTableInput.length);
 	console.log(oTableA.length);
 	for(var i = 0; i < oTableA.length; i++) {
 		oTableA[i].index = i;
 		oTableA[i].onclick = function() {
 			console.log("ok " + this.index+"--"+this.name); //点击的是第几个
 			var englishName = this.name;
-			var data = oTableInput[i].value;
+			// var data = oTableInput[i].value;
 			console.log("xjr"+data);
 			console.log("lxw "+englishName);
 			$('#myModuleAddChangeModal').modal(); //显示新建与编辑机芯机型时的弹框
