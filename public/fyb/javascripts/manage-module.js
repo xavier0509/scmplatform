d1d6da2be1933e99eae7ef6d857d8e2397eb19c3
xavier0509 <xiejr_work@163.com>
2017-01-20 18:20:10 +0800

    modify: 模块管理隐藏所有数据

--- public/fyb/javascripts/manage-module.js
+++ public/fyb/javascripts/manage-module.js
@@ -21,7 +21,7 @@ function AfterModuleHtmlInfo() {
 	console.log("xjr"+oTableInput.length);
 	console.log(oTableA.length);
 	for(var i = 0; i < oTableA.length; i++) {
-		oTableA[i].index = i;
+		// oTableA[i].index = i;
 		oTableA[i].onclick = function() {
 			console.log("ok " + this.index+"--"+this.name); //点击的是第几个
 			var englishName = this.name;
