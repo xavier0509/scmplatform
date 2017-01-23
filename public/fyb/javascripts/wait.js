6c31c99a5641b13a7d4ab1e0ded98e8fbfe5ade1
linxinwang <linxinwang@source.skyworth.com:29419>
2017-01-23 13:35:25 +0800

    lxw-012309

--- public/fyb/javascripts/wait.js
+++ public/fyb/javascripts/wait.js
@@ -595,9 +595,11 @@ function checkModelInfoInDel(){
 		var ModelInArray = jQuery.inArray(TwiceTransferModel,allModelArray);
 		if(ChipInArray==-1){
 			document.getElementById("myDeleteModalErrorInfo").style.display = "block";
+			document.getElementById("myDeleteModalErrorInfo").innerHTML = "";
 			document.getElementById("myDeleteModalErrorInfo").innerHTML = "机芯：" + TwiceTransferChip + "在数据库里不存在";
 		}else if(ChipInArray!=-1 && ModelInArray==-1){
 			document.getElementById("myDeleteModalErrorInfo").style.display = "block";
+			document.getElementById("myDeleteModalErrorInfo").innerHTML = "";
 			document.getElementById("myDeleteModalErrorInfo").innerHTML = "机型：" + TwiceTransferModel + "在数据库里不存在"
 		}
 		singleDeletePageButtons(TwiceTransferChip, TwiceTransferModel);
