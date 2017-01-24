3c23594d4ae795cadd4a7f174c721cf72fdd747d
linxinwang <linxinwang@source.skyworth.com:29419>
2017-01-24 16:27:44 +0800

    lxw-012437

--- public/v2/javascripts/wait.js
+++ public/v2/javascripts/wait.js
@@ -1547,6 +1547,7 @@ function getCopyInforesult() {
 			console.log("lxw " + "change chipinfo success");
 			if(data.msg == "success") {
 				console.log("lxw " + "访问成功");
+				var myParentObj = document.getElementById("myCopyModal");
 				//console.log("lxw " + JSON.stringify(data.data[0]));
 				document.getElementById("newCopyChip").value = data.data[0].chip;
 				document.getElementById("newCopyModel").value = data.data[0].model;
@@ -1561,6 +1562,7 @@ function getCopyInforesult() {
 					mkcounter++;
 					console.log("lxw counter = " + mkcounter + "--" + mkkey);
 					//document.getElementById(mkkey).setAttribute('checked', 'checked');
+					console.log(myParentObj);
 					myParentObj.getElementById(mkkey).setAttribute('checked', '');
 					//document.getElementById(mkkey).checked="checked";
 					console.log(myParentObj.getElementById(mkkey).checked);
