b0c4cd7a222101b146eb3ccf71d74358035e001b
linxinwang <linxinwang@source.skyworth.com:29419>
2017-02-07 14:24:40 +0800

    lxw-020719

--- public/v2/javascripts/wait.js
+++ public/v2/javascripts/wait.js
@@ -2240,7 +2240,8 @@ function moreEditPageButtons() {
 	console.log(omyTextEditObj.length);
 	for (var ii=0; ii<omyTextEditObj.length; ii++) {
 		omyTextEditObj[ii].childNodes[1].onchange = function(){
-			console.log(this.type+"---"+$(this).getAttribute("oldvalue")+"|"+this.value+"---"+this.previousSibling.title);
+			console.log(document.getElementById(this.id).getAttribute("oldvalue"));
+			console.log(this.type+"---"+this.getAttribute("oldvalue")+"|"+this.value+"---"+this.previousSibling.title);
 		}
 	}
 }
