1032298276b79e0c0676e9d68c3096cb635d2e00
linxinwang <linxinwang@source.skyworth.com:29419>
2017-02-10 17:46:34 +0800

    lxw-0210237

--- public/v2/javascripts/wait.js
+++ public/v2/javascripts/wait.js
@@ -2305,9 +2305,9 @@ function moreEditPageButtons() {
 	oButtonEditEnsure.onclick = function() {
 		console.log("批量修改页-提交取消按钮");
 		$("#myMoreEditSubmitModal").modal('hide');
-		//$("#myMoreEditModal").modal('hide');
-		//$("#myMoreEditModal").modal();
-		//$(".modal-backdrop").addClass("new-backdrop");
+		$("#myMoreEditModal").modal('toggle');
+		$("#myMoreEditModal").modal('show');
+		$(".modal-backdrop").addClass("new-backdrop");
 	}
 	/*批量修改页mk-config button的点击*/
 	functionMkConfigTable("myMoreEditModalMkButton", "myMoreEditModalMkTable", "myMoreEditModalConfigButton", "myMoreEditModalConfigTable");
