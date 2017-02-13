b20214482a66567e8ea5928b652fcb3f98603dbc
xavier0509 <xiejr_work@163.com>
2017-02-13 14:02:26 +0800

    关闭提示

--- public/v2/javascripts/review.js
+++ public/v2/javascripts/review.js
@@ -1134,12 +1134,14 @@ function freshReviewHtml() {
     iframe[0].src = "wait.html";
 }   
 
-// if(document.getElementById("closeReview")){
-    document.getElementById("closeReview").onclick=closeFun;
-// }
-// if(document.getElementById("closeReview1")){
-    document.getElementById("closeReview1").onclick=closeFun1;
-// }
+(function(){
+    if(document.getElementById("closeReview")){
+        document.getElementById("closeReview").onclick=closeFun;
+    }
+    if(document.getElementById("closeReview1")){
+        document.getElementById("closeReview1").onclick=closeFun1;
+    }
+})
 
 function closeFun(){
     console.log("用户等级："+level);
