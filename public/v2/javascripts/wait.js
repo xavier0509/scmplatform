ebc6128f918c052b4a0107d5480e94fbd6f8beb2
linxinwang <linxinwang@source.skyworth.com:29419>
2017-02-15 11:15:18 +0800

    lxw-0215013

--- public/v2/javascripts/wait.js
+++ public/v2/javascripts/wait.js
@@ -728,8 +728,9 @@ function productAddresult() {
 				console.log("lxw " + "添加成功");
 				$("#myAddModal").modal('hide');
 				$("#myCopyModal").modal('hide');
-				setTimeout("freshHtml('tab_userMenu2')", 3000);
-				setTimeout("startSelect()", 3000);
+				document.getElementById("successMessage").style.display = "block";
+				//setTimeout("freshHtml('tab_userMenu2')", 3000);
+				//setTimeout("startSelect()", 3000);
 			} else if(data.msg == "failure") {
 				console.log("lxw " + "修改失败");
 				document.getElementById("myAddModalErrorInfo").style.display = "block";
